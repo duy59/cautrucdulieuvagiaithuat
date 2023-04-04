ee6d710efd42632110d3249cf18e05de574051ba
duy59 <129411107+duy59@users.noreply.github.com>
2023-04-04 20:40:33 +0700

    Add files via upload

---
+++ xau nhi phan ke tiep.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t ; cin >> t ;
+	while(t--)
+	{
+		string s ; 
+	cin >> s ;
+    int n = s.size() - 1  ;
+    while(n >= 0 && s[n] == '1')
+    {
+    	s[n] = '0' ;
+    	--n ;
+	}
+	s[n] = '1' ;
+	cout << s << endl ; ;
+	}
+}
